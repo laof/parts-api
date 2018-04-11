19dedfaaef8c92d3af838e42ec7506e19d08203f
lovaactive <hua.long@pactera.com>
2018-04-11 18:13:25 +0800

    add mysql

--- dist/sql.service.js
+++ dist/sql.service.js
@@ -1,3 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
+const typeorm_1 = require("typeorm");
+const Photo_1 = require("./entity/Photo");
+typeorm_1.createConnection().then(async (connection) => {
+    const photo = new Photo_1.Photo();
+    photo.name = 'Me and Bears';
+    photo.description = 'I am near polar bears';
+    photo.fileName = 'photo-with-bears.jpg';
+    photo.views = 212;
+    photo.content = 'fdsafa';
+    photo.isPublished = true;
+    await connection.manager.save(photo);
+    connection.close();
+}).catch(error => {
+    console.log(11);
+});
